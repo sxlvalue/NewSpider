b82a6b2c399f243d8f28561f560bcf0e0147d609
Lewis <lewis@Lewiss-office-workstation.local>
2019-03-04 19:40:09 +0800

    start

---
+++ src/NewSpider/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewSpider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
